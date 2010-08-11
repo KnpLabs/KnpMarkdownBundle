cb085c030237a63393a8b1769732b1b316e73a0c
ornicar <thibault.duplessis@gmail.com>
2010-08-11 11:21:17 +0200

    Upgrade MarkdownBundle to latest Symfony2 style

--- MarkdownBundle.php
+++ MarkdownBundle.php
@@ -2,17 +2,9 @@
 
 namespace Bundle\MarkdownBundle;
 
-use Bundle\MarkdownBundle\DependencyInjection\MarkdownExtension;
 use Symfony\Framework\Bundle\Bundle as BaseBundle;
-use Symfony\Components\DependencyInjection\ParameterBag\ParameterBagInterface;
-use Symfony\Components\DependencyInjection\ContainerBuilder;
 
 class MarkdownBundle extends BaseBundle
 {
-
-    public function buildContainer(ParameterBagInterface $parameterBag)
-    {
-        ContainerBuilder::registerExtension(new MarkdownExtension());
-    }
 
 }
