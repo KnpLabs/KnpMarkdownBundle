6497f6263271401c6fb6043e8fc2b5c2b2e7e007
Antoine Hérault <antoine.herault@gmail.com>
2011-01-22 00:18:25 +0100

    Implement two new bundle interface methods

--- MarkdownBundle.php
+++ MarkdownBundle.php
@@ -6,5 +6,13 @@ use Symfony\Component\HttpKernel\Bundle\Bundle as BaseBundle;
 
 class MarkdownBundle extends BaseBundle
 {
+    public function getNamespace()
+    {
+        return __NAMESPACE__;
+    }
 
+    public function getPath()
+    {
+        return __DIR__;
+    }
 }
